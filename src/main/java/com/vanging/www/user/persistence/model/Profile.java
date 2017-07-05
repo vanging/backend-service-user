bb2d36da0d05a164d4457034eb41204578ff17f8
孟政元 <278227739@qq.com>
2017-07-05 23:48:19 +0800

    调整auth表和profile表对应的模型类

--- src/main/java/com/vanging/www/user/persistence/model/Profile.java
+++ src/main/java/com/vanging/www/user/persistence/model/Profile.java
@@ -1,5 +1,7 @@
 package com.vanging.www.user.persistence.model;
 
+import java.util.Date;
+
 public class Profile
 {
 
@@ -12,6 +14,11 @@ public class Profile
     private String nickname;
     private String tel;
     private String username;
+
+    // record changes
+    private Date created_at;
+    private Date deleted_at;
+    private Date updated_at;
 
     public boolean isVerified() {
         return verified;
@@ -63,5 +70,29 @@ public class Profile
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(Date deleted_at) {
+        this.deleted_at = deleted_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
     }
 }
