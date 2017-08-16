5e67ae8e8938f3b8aa0cb0de5ef5e01bdad59d83
孟政元 <278227739@qq.com>
2017-08-16 10:15:10 +0800

    /exist/email

--- src/main/java/com/vanging/www/user/restful/servlet/exist/Email.java
+++ src/main/java/com/vanging/www/user/restful/servlet/exist/Email.java
@@ -1,6 +1,8 @@
 package com.vanging.www.user.restful.servlet.exist;
 
 import com.alibaba.fastjson.JSON;
+import com.vanging.www.user.persistence.Action;
+import com.vanging.www.user.restful.response.Response;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,24 @@ public class Email extends HttpServlet
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        JSON.writeJSONString(response.getWriter(), "{}");
+        Response finalResponse = new Response();
+        String email = request.getParameter("email");
+        if(email == null)
+        {
+            finalResponse.setStatus("no_email");
+        }
+        else
+        {
+            if(Action.isEmailExist(email))
+            {
+                finalResponse.setMessage(true);
+            }
+            else
+            {
+                finalResponse.setMessage(false);
+            }
+            finalResponse.setStatus("ok");
+        }
+        JSON.writeJSONString(response.getWriter(), finalResponse);
     }
 }
